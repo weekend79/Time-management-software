8a506180662c313600dbcbb9314dea87a00ca383
weekend79 <mortenviken@yahoo.com>
2020-04-20 21:57:07 +0000

    env.py

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["MONGO_URI"] = "mongodb+srv://root:David2020@cluster0-wbu1f.mongodb.net/\
+time_manager?retryWrites=true&w=majority"
+os.environ["SECRET_KEY"] = "\xb0\xc1\x1e\x9f\x7fY\x880\xeb\tQ\xf2X\n_\xd8"
