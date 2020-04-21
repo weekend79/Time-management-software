c19558ce2c253d98203005e0619a106ba88dcf91
weekend79 <mortenviken@yahoo.com>
2020-04-21 19:56:07 +0000

    added the secret key to my run.app

--- app.py
+++ app.py
@@ -230,6 +230,7 @@ def project_profile():
 
 # App run
 if __name__ == '__main__':
+    app.secret_key = os.getenv('SECRET_KEY')
     app.run(host=os.environ.get('IP', '0.0.0.0'),
             port=int(os.environ.get('PORT', '5000')),
             debug=True)
